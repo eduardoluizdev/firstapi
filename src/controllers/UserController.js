04204d246bb9b7f892947e7a899d7fa5d57a932e
Eduardo Luiz <hello@eduardoluiz.dev>
2021-11-17 10:47:30 -0300

    wip

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,19 @@ const users = require("../mock/users");
 
 module.exports = {
   listUsers(req, res) {
+    const { order } = req.query;
+
+    console.log(order);
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+
+      return a.id > b.id ? 1 : -1;
+    });
+
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
+    res.end(JSON.stringify(sortedUsers));
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const http = require("http");
+const { URL } = require("url");
 
 const routes = require("./routes");
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(`http://localhost:3000${req.url}`);
+
   const route = routes.find(
     (routerObj) =>
-      routerObj.endpoint === req.url && routerObj.method === req.method
+      routerObj.endpoint === parsedUrl.pathname &&
+      routerObj.method === req.method
   );
 
   if (route) {
+    req.query = Object.fromEntries(parsedUrl.searchParams);
+
     route.handler(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
-    res.end(`Cannot ${req.method} ${req.url}`);
+    res.end(`Cannot ${req.method} ${parsedUrl.pathname}`);
   }
 });
 
