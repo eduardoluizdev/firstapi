a99c0bd265b5ad365d7f72ccaa66a7dd16bd1f72
Eduardo Luiz <hello@eduardoluiz.dev>
2021-11-23 07:25:06 -0300

    feat: create users endpoints to list and create users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,8 +4,6 @@ module.exports = {
   listUsers(req, res) {
     const { order } = req.query;
 
-    console.log(order);
-
     const sortedUsers = users.sort((a, b) => {
       if (order === "desc") {
         return a.id < b.id ? 1 : -1;
@@ -14,7 +12,30 @@ module.exports = {
       return a.id > b.id ? 1 : -1;
     });
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(sortedUsers));
+    res.send(200, sortedUsers);
+  },
+  getUserById(req, res) {
+    const { id } = req.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      return res.send(400, { error: "User not found" });
+    }
+
+    res.send(200, user);
+  },
+  createUser(req, res) {
+    const { body } = req;
+
+    const lastUserId = users[users.length - 1].id;
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    };
+
+    users.push(newUser);
+
+    res.send(200, body);
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const http = require("http");
 const { URL } = require("url");
 
+const bodyParser = require("./helpers/bodyParser");
 const routes = require("./routes");
 
 const server = http.createServer((req, res) => {
   const parsedUrl = new URL(`http://localhost:3000${req.url}`);
 
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const sliptEndPoint = pathname.split("/").filter(Boolean);
+
+  if (sliptEndPoint.length > 1) {
+    pathname = `/${sliptEndPoint[0]}/:id`;
+    id = sliptEndPoint[1];
+  }
+
   const route = routes.find(
     (routerObj) =>
-      routerObj.endpoint === parsedUrl.pathname &&
-      routerObj.method === req.method
+      routerObj.endpoint === pathname && routerObj.method === req.method
   );
 
   if (route) {
     req.query = Object.fromEntries(parsedUrl.searchParams);
+    req.params = { id };
 
-    route.handler(req, res);
+    res.send = (statusCode, body) => {
+      res.writeHead(statusCode, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(req.method)) {
+      bodyParser(req, () => route.handler(req, res));
+    } else {
+      route.handler(req, res);
+    }
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end(`Cannot ${req.method} ${parsedUrl.pathname}`);
